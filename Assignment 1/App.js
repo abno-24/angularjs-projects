15f0d4b18c7c75a7620938f77fefd2f5bbc1d383
Abno <abnomaurya98@gmail.com>
2023-03-12 21:19:46 +0530

    First Assignment Submission

---
+++ Assignment 1/App.js
@@ -0,0 +1,20 @@
+angular.module('myFirstAsgnmt', [])
+.controller('Ctrl', function($scope){
+    $scope.items = "";
+
+    $scope.itemChecker = function (itemsToCount, separator) {
+        itemsToCount = $scope.items;
+        separator = ",";
+        const arrOfItems = itemsToCount.split(separator);
+        var count = arrOfItems.length;
+        console.log(count);
+
+        if(count <= 3){
+            $scope.Msg = "Enjoy!"
+        }else if(count > 3){
+            $scope.Msg = "Too Much"
+        }else{
+            $scope.Msg = ""
+        }
+    }
+})
